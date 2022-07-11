85019a68ab836cc1dd9979d014293e5e18075787
anie308 <aniebietabasiobo@gmail.com>
2022-07-11 09:55:57 +0100

    changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,6 @@ import { AiOutlineMenu } from "react-icons/ai";
 import { FaTimes } from "react-icons/fa";
 import { navLinks, socials } from "../data";
 import { Link } from "react-router-dom";
-import {useMatch} from 'react-router-dom'
 
 function NavBar({to}) {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,26 +11,9 @@ function NavBar({to}) {
     setIsOpen(!isOpen);
   };
 
-     let match = useMatch(to !== "" ? `/` + to + "/*" : "/");
 
 
-  // function CustomLink({ children, to, menu, showsidebar, ...props }: any) {
-  //   let match = useMatch(to !== "" ? `/dashboard/` + to + "/*" : "/dashboard");
-  //   return (
-  //     <Link
-  //       match={match}
-  //       className={`  ${
-  //         match
-  //           ? "  sm:bg-[#0156da1a]  text-primary sm:border-r-[5px] sm:border-blue-500"
-  //           : "text-[#AEAEAE] bg-white"
-  //       }  sm:pl-20px w-[30px] sm:w-full text-lg flex flex-col sm:flex-row items-center mx-[20px] sm:mx-[5px] sm:m-0 justify-center sm:justify-start   md:mt-2  sm:p-[16px] `}
-  //       to={to}
-  //       {...props}
-  //     >
-  //       {children}
-  //     </Link>
-  //   );
-  // }
+
 
 
   return (
@@ -61,16 +43,15 @@ function NavBar({to}) {
         <div className="flex flex-col justify-between items-center space-y-12 pt-[75px] md:pt-0 md:flex-row">
 
       <div className='flex flex-col md:flex-row justify-center items-center h-full md:h-fit space-y-4  md:space-y-0'>
-      {navLinks.map(({ id, name, path, className, match }) => (
+      {navLinks.map(({ id, name, path, className }) => (
             <div key={id}>
               <Link
-              match={match}
               onClick={handleMenu}
                 to={path}
                 className={`${
                   // eslint-disable-next-line no-sequences
-                  className ? className : "", 
-               match? 'border-b-[3px]' : '' }  font-[500] text-desktoplink text-[18px] sm:text-[16px]  lg:text-[18px] md:ml-[20px] font-lato  `}
+                  className ? className : ""
+                }  font-[500] text-desktoplink text-[18px] sm:text-[16px]  lg:text-[18px] md:ml-[20px] font-lato  `}
               >
                 {name}
               </Link>
