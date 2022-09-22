eaab51e634b709333401fdc846c470279b427a98
anie308 <aniebietabasiobo@gmail.com>
2022-09-22 10:26:45 +0100

    config

--- src/components/Lisitings/BiteListing.jsx
+++ src/components/Lisitings/BiteListing.jsx
@@ -13,10 +13,9 @@ function BiteListing() {
 
 return (
   <div>
-        {isLoading && <div className='min-w-[280px] max-w-[300]   min-h-[370px] h-[370px] animate-pulse flex flex-col space-y-2'>
-          <div className='rounded-[10px] w-full  h-[200px] bg-gray-200'></div>
-          <div className='rounded-[10px] w-full  h-full bg-gray-200'></div>
-          <div className='rounded-[10px] w-full  h-[80px] bg-gray-200'></div>
+        {isLoading && <div className='w-full  bg-gray-200   h-[300px] animate-pulse flex items-center justify-center rounded-[10px] font-raleway font-[700] text-[20px]'>
+          Loading...
+         
         </div> }
         {error && <div className='h-[200px] w-full bg-gray-200 rounded-[10px] font-raleway font-[700] text-[20px] flex items-center justify-center'>Something went wrong </div>}
         {isSuccess && (
